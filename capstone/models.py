37b6a65b27c52e8746d618609bcde532f8800001
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-09 14:39:28 -0600

    migrated user feedback table, had to drop datetime b/c django utils import timezone was throwing error

---
+++ capstone/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=250)
+    category = models.CharField(max_length=50, default='General')
+    email = models.CharField(max_length=150)
+    comment = models.CharField(max_length=1000)
+    has_read = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'feedback'
+
+
