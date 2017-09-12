258fedd553da4d23065f15b61fd80914f53963af
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-12 08:10:27 -0600

    feedback app is working, create more routes

--- capstone/urls.py
+++ capstone/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import url
-from . admin import my_admin_site
+from django.conf.urls import url, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    # url(r'^time/', views.current_datetime),
-    url(r'^$', views.index),
-    url(r'^admin/', my_admin_site.urls),
-    # url(r'^employees/(?P<year>[0-9]{4})/$', views.year_joined, name='capstone')
+    url(r'^admin/', admin.site.urls),
+    url(r'^employees/(?P<pk>[0-9]{4})/profile/$', views.my_profile, name='capstone')
 ]
--- feedback/forms.py
+++ feedback/forms.py
@@ -4,12 +4,14 @@ from django.core.validators import EmailValidator, MinLengthValidator, MaxLength
 
 
 class FeedbackAddForm(forms.ModelForm):
-    email = forms.CharField(widget=forms.TextInput(attrs={'size': 20}), validators=[EmailValidator(), MinLengthValidator(6), MaxLengthValidator(100)])
+    email = forms.CharField(widget=forms.TextInput(attrs={'size': 20}),
+                            validators=[EmailValidator(), MinLengthValidator(6), MaxLengthValidator(100)])
     comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 30, 'style': 'resize:none;'}))
 
     class Meta:
         model = Feedback
         fields = ('name', 'subject', 'category', 'email', 'comment', 'has_read',)
+
 
 class FeedbackForm(forms.ModelForm):
     comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 30, 'style': 'resize:none;'}))
